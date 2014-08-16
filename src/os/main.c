0c8f53a81b0dd06dc99247b6e848d4921eb9410e
yuseinishiyama <allegllet.scherzand@gmail.com>
2014-08-16 22:58:56 +0900

    Add sample programs using inter-threads communication.

--- src/os/main.c
+++ src/os/main.c
@@ -11,7 +11,9 @@ static int start_threads(int argc, char *argv[]) {
   /* test09_1_id = kz_run(test09_1_main, "test09_1", 1, 0x100, 0, NULL); */
   /* test09_2_id = kz_run(test09_2_main, "test09_2", 2, 0x100, 0, NULL); */
   /* test09_3_id = kz_run(test09_3_main, "test09_3", 3, 0x100, 0, NULL); */
-  kz_run(test10_1_main, "test10_1", 1, 0x100, 0, NULL);
+  /* kz_run(test10_1_main, "test10_1", 1, 0x100, 0, NULL); */
+  kz_run(test11_1_main, "test11_1", 1, 0x100, 0, NULL);
+  kz_run(test11_2_main, "test11_2", 1, 0x100, 0, NULL);
 
   kz_chpri(15);
   INTR_ENABLE;
